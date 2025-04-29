c49852ba250c544292bec41a9384fc70a0bb46ef
eg-martins <ed.world.martins@gmail.com>
2025-04-29 11:56:18 -0300

    Create animacoes.js

---
+++ js/animacoes.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const botao = document.getElementById("botao");
+  botao.addEventListener("click", () => {
+    alert("Você clicou no botão! 🎉");
+  });
+});
